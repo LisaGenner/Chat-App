199b93b0010489381bfb9619bb8d73aaa461a428
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-05-30 14:16:39 -0500

    5.1 added name to chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,38 @@
 import React from 'react';
+import { useEffect } from "react";
 import { StyleSheet, View, Text } from 'react-native';
 
 const Chat = ({route, navigation }) => {
-    const { color, name } = route.params;
+    const { name, color } = route.params;
+    useEffect(() => {
+      navigation.setOptions({ title: name });
+    }, []);
 
-    console.log(color, name);
-    
- return (
-   <View style={[styles.container, { backgroundColor: color
+   
+return (
+    <View style={[styles.container, { backgroundColor: color
    }]}>
-     <Text>Chat</Text>
-   </View>
- );
-}
+     <Text
+        style={
+          color !== "white" ? [{ color: "white" }, styles.title] : styles.title
+        }
+      >
+        Let's Chat
+      </Text>
+    </View>
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
   }
 });    
 
