b086d2938c422a797e574b13c3a19e90c413fa39
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-05-30 20:56:10 -0500

    5.2 installed gifted chat

--- App.js
+++ App.js
@@ -10,25 +10,27 @@ import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-//Creat the navigator
+//Create the navigator
 const Stack = createNativeStackNavigator();
 
 // The app’s main Chat component that renders the chat UI
-export default class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Start'>
-          <Stack.Screen
-            name='Start'
-            component={Start}
-          />
-          <Stack.Screen
-            name='Chat'
-            component={Chat}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-}
+
+export default App;
--- components/Chat.js
+++ components/Chat.js
@@ -1,28 +1,75 @@
 import React from 'react';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import {KeyboardAvoidingView, Platform } from 'react-native';
 
 const Chat = ({route, navigation }) => {
     const { name, color } = route.params;
+    const [messages, setMessages] = useState([]);
+    
     useEffect(() => {
       navigation.setOptions({ title: name });
+      setMessages([
+        {
+          _id: 1,
+          text: "Hello Developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+       {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+      ]);
     }, []);
 
-   
-return (
-    <View style={[styles.container, { backgroundColor: color
-   }]}>
-     <Text
-        style={
-          color !== "white" ? [{ color: "white" }, styles.title] : styles.title
+    const onSend = (newMessages) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+    const renderBubble = (props) => {
+      return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
         }
-      >
-        Let's Chat
-      </Text>
-    </View>
-  );
-};
+      }}
+      />
+    }
 
+return (
+  <View style={[styles.container, { backgroundColor: color
+  }]}>
+     <GiftedChat 
+  messages={messages}
+  renderBubble={renderBubble}
+  onSend={messages => onSend(messages)}
+  user={{
+    _id: 1
+  }}
+  />
+  {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+  {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
+    <Text
+  style={
+   color !== "white" ? [{ color: "white" }, styles.title] : styles.title
+ }
+>
+ Let's Chat
+ </Text>
+</View>
+);
+};
 
 const styles = StyleSheet.create({
   container: {
